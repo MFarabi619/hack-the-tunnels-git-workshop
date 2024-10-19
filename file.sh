8818b41e48408e3f0b0199e6567db3b5eb36430c
Mumtahin Farabi <mfarabi619@gmail.com>
2024-10-18 20:33:55 -0400

    initial commit

---
+++ file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Hack the Tunnels"
+cowsay "Hack the Tunnels" | lolcat
+cowsay "Hack the Tunnels" | lolcat
+cowsay "Hack the Tunnels" | lolcat
+cowsay "Hack the Tunnels" | lolcat
+cowsay "Hack the Tunnels" | lolcat
